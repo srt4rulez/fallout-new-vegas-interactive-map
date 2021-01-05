a3767012708fccc0331dce55fdd4fc710557e6a0
Jake Dennison <srt4rulez@gmail.com>
2021-01-04 21:31:57 -0500

    Add MarkerList component and update MarkerTypePanel with sub types

--- src/Components/MarkerTypePanel/MarkerTypePanel.js
+++ src/Components/MarkerTypePanel/MarkerTypePanel.js
@@ -6,7 +6,9 @@ import {
     typeMap,
     typeLabelMap,
     typeColorMap,
+    subTypeSkillBookLabelMap,
 } from 'Data/marker-types';
+import MarkerListItem from 'Components/MarkerListItem/MarkerListItem';
 
 const propTypes = {
     className: PropTypes.string,
@@ -26,7 +28,32 @@ const defaultProps = {
     onMarkerTitleClick: (marker = {}) => (event) => {},
 };
 
+const typesThatHaveSubTypes = [
+    typeMap.SkillBook,
+];
+
 const MarkerTypePanel = (props) => {
+
+    const markers = props.markers;
+
+    const hasSubTypes = typesThatHaveSubTypes.includes(props.type);
+
+    const subTypes = {};
+
+    if (hasSubTypes) {
+        markers.forEach((marker) => {
+            if (subTypes[marker.sub_type]) {
+                // Already a object, just add the new marker to markers.
+                subTypes[marker.sub_type].markers.push(marker);
+            } else {
+                // Create object for sub type
+                subTypes[marker.sub_type] = {
+                    id: marker.sub_type,
+                    markers: [marker],
+                };
+            }
+        });
+    }
 
     return (
 
@@ -67,33 +94,60 @@ const MarkerTypePanel = (props) => {
                 className={classNames('marker-type-panel__list')}
             >
 
-                {props.markers.map((marker) => {
+                {(hasSubTypes) ? Object.values(subTypes).map((subType) => {
 
                     return (
 
                         <li
-                            key={marker.id}
-                            className={classNames('marker-type-panel__list-item')}
+                            className={classNames('marker-type-panel__sub-type-list-item')}
+                            key={subType.id}
                         >
 
-                            <input
-                                className={classNames('marker-type-panel__item-checkbox')}
-                                type="checkbox"
-                                checked={marker.isFound}
-                                onChange={props.onMarkButtonClick(marker)}
-                                title="Mark As Found"
-                            />
-
-                            <button
-                                className={classNames('marker-type-panel__item-link')}
-                                onClick={props.onMarkerTitleClick(marker)}
+                            <span
+                                className={classNames('marker-type-panel__sub-type-label')}
                             >
 
-                                {marker.title}
+                                {subTypeSkillBookLabelMap[subType.id]}
 
-                            </button>
+                            </span>
+
+                            <ul>
+
+                                {subType.markers.map((marker) => {
+
+                                    return (
+
+                                        <MarkerListItem
+                                            tag="li"
+                                            key={marker.id}
+                                            isFound={marker.isFound}
+                                            onMarkCheckboxChange={props.onMarkButtonClick(marker)}
+                                            onMarkerTitleClick={props.onMarkerTitleClick(marker)}
+                                            title={marker.title}
+                                        />
+
+                                    );
+
+                                })}
+
+                            </ul>
 
                         </li>
+
+                    );
+
+                }) : props.markers.map((marker) => {
+
+                    return (
+
+                        <MarkerListItem
+                            tag="li"
+                            key={marker.id}
+                            isFound={marker.isFound}
+                            onMarkCheckboxChange={props.onMarkButtonClick(marker)}
+                            onMarkerTitleClick={props.onMarkerTitleClick(marker)}
+                            title={marker.title}
+                        />
 
                     );
 
