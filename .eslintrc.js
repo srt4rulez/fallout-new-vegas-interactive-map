f37f4158c5355b4ffe944b85b158d8a15ccc3d9c
Jake Dennison <srt4rulez@gmail.com>
2021-09-19 21:22:31 -0400

    Move indented comment line in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,7 @@ module.exports = {
     },
     'settings': {
         'react': {
-            'createClass': 'createReactClass', // Regex for Component Factory to use,
-                                               // default to 'createReactClass'
+            'createClass': 'createReactClass', // Regex for Component Factory to use, defaults to 'createReactClass'
             'pragma':   'React',  // Pragma to use, default to 'React'
             'fragment': 'Fragment',  // Fragment to use (may be a property of <pragma>), default to 'Fragment'
             'version':  'detect', // React version. 'detect' automatically picks the version you have installed.
