bb3599530a9a15f50e6e9d40633d938a77596da7
Jake Dennison <srt4rulez@gmail.com>
2021-10-23 19:29:22 -0400

    Add chakra ui packages and update index.tsx with provider

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import 'leaflet-defaulticon-compatibility';
 import './index.scss';
 import App from 'Components/App/App';
+import { ChakraProvider } from '@chakra-ui/react';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <ChakraProvider>
+            <App />
+        </ChakraProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
