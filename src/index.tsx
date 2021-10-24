a08df67e5039fcf363b4c6293a13b8a4f66a2bcd
Jake Dennison <srt4rulez@gmail.com>
2021-10-24 13:50:00 -0400

    Add theme file for chakra, replace robot font link

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,15 @@ import 'leaflet-defaulticon-compatibility';
 import './index.scss';
 import App from 'Components/App/App';
 import { ChakraProvider } from '@chakra-ui/react';
+import theme from './theme';
+import '@fontsource/roboto';
+import '@fontsource/roboto/700.css';
 
 ReactDOM.render(
     <React.StrictMode>
-        <ChakraProvider>
+        <ChakraProvider
+            theme={theme}
+        >
             <App />
         </ChakraProvider>
     </React.StrictMode>,
---
+++ src/theme.ts
@@ -0,0 +1,10 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+    fonts: {
+        heading: 'Roboto',
+        body: 'Roboto',
+    },
+});
+
+export default theme;
