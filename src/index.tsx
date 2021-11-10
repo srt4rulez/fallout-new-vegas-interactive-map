818c3259d425d7ee0d2746b8980ec6b33f9a5fc0
Jake Dennison <srt4rulez@gmail.com>
2021-11-09 21:02:43 -0500

    Update index.tsx with redux provider

--- src/index.tsx
+++ src/index.tsx
@@ -7,14 +7,18 @@ import { ChakraProvider } from '@chakra-ui/react';
 import theme from './theme';
 import '@fontsource/roboto';
 import '@fontsource/roboto/700.css';
+import { Provider } from 'react-redux';
+import store from './store';
 
 ReactDOM.render(
     <React.StrictMode>
-        <ChakraProvider
-            theme={theme}
-        >
-            <App />
-        </ChakraProvider>
+        <Provider store={store}>
+            <ChakraProvider
+                theme={theme}
+            >
+                <App />
+            </ChakraProvider>
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
 );
