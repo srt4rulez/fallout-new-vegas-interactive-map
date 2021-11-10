7f0e5a8a6758a627397d52b9226ac2e9ff176586
Jake Dennison <srt4rulez@gmail.com>
2021-11-09 21:02:20 -0500

    Add store.ts and hooks.ts

---
+++ src/store.ts
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit';
+import appReducer from './Slices/appSlice';
+
+const store = configureStore({
+    reducer: {
+        app: appReducer,
+    },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
