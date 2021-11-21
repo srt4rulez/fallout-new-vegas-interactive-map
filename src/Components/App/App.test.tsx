f4ac44dc9203c3512af28bf18810b16e0e9fdb0c
Jake Dennison <srt4rulez@gmail.com>
2021-11-20 21:04:52 -0500

    Remove obsolete App.test.tsx

--- src/Components/App/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from 'Components/App/App';
-
-test('renders app with "Interactive Map" text', () => {
-    render(<App />);
-    const linkElement = screen.getByText(/Interactive Map/i);
-    expect(linkElement).toBeInTheDocument();
-});
